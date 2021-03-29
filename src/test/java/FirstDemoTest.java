5f19d046efc55d641df8d983078b40d9ed072786
tkmoney11 <tylerkim@outlook.com>
2021-03-29 13:31:48 -0700

    Only successful selectors are absolute and relative xpath so far

--- src/test/java/FirstDemoTest.java
+++ src/test/java/FirstDemoTest.java
@@ -16,11 +16,11 @@ public class FirstDemoTest {
         webDriver = new ChromeDriver();
         webDriver.get(url);
     }
-
-    @Test
-    public void test() throws InterruptedException {
-        System.out.println(webDriver.getTitle());
-    }
+//
+//    @Test
+//    public void test() {
+//        System.out.println(webDriver.getTitle());
+//    }
 
     @Test
     public void testLocators() {
@@ -28,26 +28,27 @@ public class FirstDemoTest {
         System.out.println("webDriver.findElement(By.name()) is also hard to find on large webapps.");
         System.out.println("webDriver.findElement(By.className()) is hard to find on webapps bc of hashing.");
         System.out.println("webDriver.findElement(By.linkText(\"Google Search\")).click(): tbh unsuccessful");
-        webDriver.findElement(By.linkText("Google Search")).click();
+//        webDriver.findElement(By.linkText("Google Search")).click();
         System.out.println("webDriver.findElement(By.partialLinkText(\"Google Search\")).click(): tbh unsuccessful");
-        webDriver.findElement(By.partialLinkText("Google")).click();
+//        webDriver.findElement(By.partialLinkText("Google")).click();
         // CSS SELECTORS:
         // tag and id: "tag#ID":"input#searchbar"
         System.out.println("cannot find tag with id");
         // tag and class: "tag.class":"input.inputbar"
-        webDriver.findElement(By.cssSelector("input.gLFyf gsfi")).sendKeys("FINALLY???");
+//        webDriver.findElement(By.cssSelector("input.gLFyf gsfi")).sendKeys("FINALLY???");
         // tag and attribute: "tag[attribute=value]" : "input[label="Search"]"
-        System.out.println(webDriver.findElement(By.cssSelector("input[label=Search]")).getLocation());
+//        System.out.println(webDriver.findElement(By.cssSelector("input[label=Search]")).getLocation());
         // tag and class and attribute: "tag.class[attribute=value]":"input.text[label=Search]"
         System.out.println("Absolute xpath");
         webDriver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input")).sendKeys("Xpath sender");
         System.out.println("Relative xpath");
-        webDriver.findElement(By.xpath("//*[@title=`Search`]")).sendKeys("relative xpath");
-        System.out.println();
+        webDriver.findElement(By.xpath("//*[@title='Search']")).sendKeys("relative xpath");
+//        webDriver.navigate().to("http://www.practiceselenium.com/check-out.html");
     }
 
     @After
     public void tearDown() {
 //        webDriver.close();
+//        webDriver.quit();
     }
 }
